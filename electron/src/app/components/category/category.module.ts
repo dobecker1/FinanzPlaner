352d550bbb188c9daae84b2bdc3a2adc775d7636
Becker <dobe@softwareag.com>
2019-07-05 15:32:40 +0200

    tree alternative for categories

--- electron/src/app/components/category/category.module.ts
+++ electron/src/app/components/category/category.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from "@angular/core";
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTreeModule } from '@angular/material/tree';
 
 //Category Components
 import { CategoryFormComponent } from './category-form/category-form.component';
@@ -13,7 +14,8 @@ import { CategoryComponent } from './categoryPage/category.component';
     imports: [
         MatButtonModule,
         MatIconModule,
-        MatGridListModule
+        MatGridListModule,
+        MatTreeModule
     ],
     declarations: [
         CategoryFormComponent,
--- electron/src/app/components/category/categoryPage/category.component.ts
+++ electron/src/app/components/category/categoryPage/category.component.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import { Category } from "src/app/models/category";
+import { FlatTreeControl } from "@angular/cdk/tree";
+import { MatTreeFlattener, MatTreeFlatDataSource } from "@angular/material";
 
 @Component({
     selector: 'category',
@@ -7,5 +10,60 @@ import { Component } from "@angular/core";
 })
 export class CategoryComponent {
 
-    
+    private transformer = (node: CategoryNode, level: number) => {
+        return {
+            expandable: !!node.children && node.children.length > 0,
+            name: node.name,
+            level: level
+        };
+    }
+
+    treeControl = new FlatTreeControl<ExampleFlatNode>(
+        node => node.level, node => node.expandable
+    );
+
+    treeFlattener = new MatTreeFlattener(
+        this.transformer, node => node.level, node => node.expandable, node => node.children
+    );
+
+    dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+    constructor() {
+        this.dataSource.data = CATEGORY_DATA;
+    }
+
+    hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
 }
+
+interface CategoryNode {
+    name: string;
+    children?: CategoryNode[];
+}
+
+interface ExampleFlatNode {
+    expandable: boolean;
+    name: string;
+    level: number;
+}
+
+const CATEGORY_DATA: CategoryNode[] = [
+    {
+        name: "Auto",
+        children: [
+            {name: "Opel Corsa"},
+            {name: "VW Golf GTE"},
+            {name: "VW Passat"}
+        ]
+    },
+    {
+        name: "Computerspiele",
+        children: [
+            {name: "Magic Arena"},
+            {name: "World of Warcraft"},
+            {name: "Hearthstone"}
+        ]
+    },
+    {
+        name: "Softare AG"
+    }
+]

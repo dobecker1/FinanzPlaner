0fd20b3a77f85f04889e82c83e738af7d9c1c4d2
Becker <dobe@softwareag.com>
2019-10-01 15:46:24 +0200

    added ledger form component - start

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LedgerFormComponent from "./ledgers/components/ledger-form-component";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <LedgerFormComponent/>
     </div>
   );
 }
---
+++ ui/src/ledgers/components/ledger-form-component.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from "@material-ui/core/Card/Card";
+import CardContent from "@material-ui/core/CardContent/CardContent";
+
+const LedgerFormComponent: React.FC = () => {
+    return(
+        <Card>
+            <CardContent>
+                Hallo Welt
+            </CardContent>
+        </Card>
+    )
+}
+
+export default LedgerFormComponent;
